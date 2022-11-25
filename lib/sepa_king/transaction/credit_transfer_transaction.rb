67b843d4900caab2e332c1ccffb3a842493faa68
richisp <mrrewez@gmail.com>
2022-11-25 10:17:29 +0200

    Add NURG

--- lib/sepa_king/transaction/credit_transfer_transaction.rb
+++ lib/sepa_king/transaction/credit_transfer_transaction.rb
@@ -5,7 +5,7 @@ module SEPA
                   :creditor_address,
                   :category_purpose
 
-    validates_inclusion_of :service_level, :in => %w(SEPA URGP), :allow_nil => true
+    validates_inclusion_of :service_level, :in => %w(NURG SEPA URGP), :allow_nil => true
     validates_length_of :category_purpose, within: 1..4, allow_nil: true
 
     validate { |t| t.validate_requested_date_after(Date.today) }
@@ -18,7 +18,9 @@ module SEPA
     def schema_compatible?(schema_name)
       case schema_name
       when PAIN_001_001_03
-        !self.service_level || (self.service_level == 'SEPA' && self.currency == 'EUR')
+        !self.service_level ||
+          (self.service_level == 'NURG') ||
+          (self.service_level == 'SEPA' && self.currency == 'EUR')
       when PAIN_001_002_03
         self.bic.present? && self.service_level == 'SEPA' && self.currency == 'EUR'
       when PAIN_001_003_03
